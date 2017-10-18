6752a04e553617c19e0bc1182fa6b56859b8449d
C Ferrell <ferrellchristianr@gmail.com>
2017-10-17 23:24:29 -0500

    this space station if fully operational! (your Rebel friends are dying)

--- assets/js/apiapp.js
+++ assets/js/apiapp.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //global vars
-    var topicArray = ["gi joe", "Nintendo", "He-Man", "Thundercats", "Alf", "Pac-Man", "DuckTales", "Voltron" ] //80's nostalgia
+    var topicArray = ["G.I. Joe", "Nintendo", "He-Man", "Thundercats", "Motley Crue", "Pac-Man", "DuckTales", "Voltron", "Hulkamania" ] //80's nostalgia
     var currentTopic = "";
     var apiKeyURL = "https://api.giphy.com/v1/gifs/search?api_key=OOnfnFi6GR7YTlFGpGbxmYlaQa6JnMwc&q="; //easier than remembering it
 
@@ -13,8 +13,8 @@ $(document).ready(function(){
             //console.log(topicArray[i]);
             var topicBtn = $('<button/>').attr("id", topicArray[i]).attr("class", "select btnStyle").text(topicArray[i]);
             $("#buttonRow").append(topicBtn);
-            };
         };
+    };
     
     setBtns();
 
@@ -22,7 +22,6 @@ $(document).ready(function(){
         //then create buttons again
     $("#add-nos").on("click", function(event) {
         event.preventDefault(); //without this line, everything was resetting... why?
-
         var nostalgia = ($("#input80s").val().trim());
         //add it to the list
         topicArray.push(nostalgia);
@@ -36,9 +35,10 @@ $(document).ready(function(){
         $("#thingBox").html("");
         //Pull id
         currentTopic = $(this).attr("id");
-        //console.log(currentTopic);
+        //random 1-20 number for the offset - since these are not current event items if you go too high you can find your images getting "out of focus"
+        var randNum = Math.floor((Math.random() * 20) + 1);
         //set Object
-        var currObj = apiKeyURL + currentTopic + "&limit=10&offset=0&rating=PG&lang=en";
+        var currObj = apiKeyURL + currentTopic + "&limit=10&offset=" + randNum + "&rating=PG&lang=en";
         console.log(currObj);
         
         //function to populate 10 pictures
@@ -52,20 +52,24 @@ $(document).ready(function(){
                 //set the fixed H, still Img to a var
                 var stillImg = response.data[i].images.fixed_height_still.url;
                 var moveImg = response.data[i].images.fixed_height.url;
+                var picRate = response.data[i].rating;
                 //make the Img a 'thing', add the motion/still data
                 var thisGiphy = $("<img>").attr("src", stillImg).attr("ani-still", stillImg).attr("ani-move", moveImg).attr("animate", "still").attr("class", "movement");
                 //add the thing to the box of things
                 $("#thingBox").prepend(thisGiphy);
+                //now to get the rating on there
+                var thisRate = $("<div>").text("Rated: " + picRate);
+                //add the thing to the box of things
+                $("#thingBox").prepend(thisRate);
             };   
         });
     });
 
     //function to swap animated with still
     $(document.body).on("click", ".movement", function() {
-        console.log("clicky");
-
+        //set the animate data
         var ani = $(this).attr("animate");
-
+        //check and flip the Animate data
         if (ani === "still") {
           $(this).attr("src", $(this).attr("ani-move")).attr("animate", "moving");
         } 
